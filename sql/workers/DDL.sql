35432e3cd8803c9eeb5b38699c230f21421ae627
Nikita Piskarev <npiskarev23@gmail.com>
2023-01-03 14:43:30 +0200

    Add initial tambe

---
+++ sql/workers/DDL.sql
@@ -0,0 +1,8 @@
+--фаил для создания таблиц и огратничений  для их полей
+CREATE TABLE workers (
+    id serial PRIMARY KEY,
+    birthday date NOT NULL,
+    name varchar(100) NOT NULL CHECK (name != ''),
+    salary numeric NOT NULL CHECK (salary > 0),
+    UNIQUE (birthday, name)
+);
